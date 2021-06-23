117f91b06c1bf0634a160adb815c6ca3e3eaec1b
José Lucas <jose.lukass03@gmail.com>
2021-06-23 18:38:52 -0300

    :sparkles: Using ensure middleware

--- src/routes/tags.routes.ts
+++ src/routes/tags.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+
 import { CreateTagsController } from '../controllers/CreateTagsController';
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 
 const routes = Router();
 
 const createTagsController = new CreateTagsController();
 
-routes.post('/tags', createTagsController.handle);
+routes.post('/tags', ensureAdmin, createTagsController.handle);
 
 export default routes;
