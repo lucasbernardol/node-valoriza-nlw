532856e3ba869a1c28028f81abe900aa1f9847c8
José Lucas <jose.lukass03@gmail.com>
2021-06-23 14:51:25 -0300

    :sparkles: Create user entity

---
+++ src/entities/User.ts
@@ -0,0 +1,39 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { v4 as uuid4 } from 'uuid';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid4();
+    }
+  }
+}
