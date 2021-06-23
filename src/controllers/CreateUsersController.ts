d8c0e4b5933868adab3a3412f02ffb882773baed
José Lucas <jose.lukass03@gmail.com>
2021-06-23 16:08:55 -0300

    :recycle: Refactoring controllers

--- src/controllers/CreateUsersController.ts
+++ src/controllers/CreateUsersController.ts
@@ -3,21 +3,17 @@ import { CreateUserService } from '../services/CreateUsersService';
 
 export class CreateUsersController {
   async create(request: Request, response: Response) {
-    try {
-      const { name, email, password, isAdmin } = request.body;
+    const { name, email, password, isAdmin } = request.body;
 
-      const createUserService = new CreateUserService();
+    const createUserService = new CreateUserService();
 
-      const created = await createUserService.create({
-        name,
-        email,
-        password,
-        isAdmin,
-      });
+    const created = await createUserService.create({
+      name,
+      email,
+      password,
+      isAdmin,
+    });
 
-      return response.json(created);
-    } catch (error) {
-      console.warn(error);
-    }
+    return response.json(created);
   }
 }
