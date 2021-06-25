ef079df11f78e567de657dfe1833b0bed29de7f4
José Lucas <jose.lukass03@gmail.com>
2021-06-25 19:37:55 -0300

    :recycle: Routes refactoring

--- src/routes/compliments.routes.ts
+++ src/routes/compliments.routes.ts
@@ -2,10 +2,16 @@ import { Router } from 'express';
 
 import { CreateComplimentsController } from '../controllers/CreateComplimentsController';
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 const routes = Router();
 
 const createComplimentsController = new CreateComplimentsController();
 
-routes.post('/compliments', createComplimentsController.handle);
+routes.post(
+  '/compliments',
+  ensureAuthenticated(),
+  createComplimentsController.handle
+);
 
 export default routes;
--- src/routes/tags.routes.ts
+++ src/routes/tags.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 
 import { CreateTagsController } from '../controllers/CreateTagsController';
+
 import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const routes = Router();
 
 const createTagsController = new CreateTagsController();
 
-routes.post('/tags', ensureAdmin, createTagsController.handle);
+routes.post(
+  '/tags',
+  ensureAuthenticated(),
+  ensureAdmin(),
+  createTagsController.handle
+);
 
 export default routes;
