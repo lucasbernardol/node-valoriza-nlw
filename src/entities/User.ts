fcd0dec59578aabf4dc90d65ef9dc1c33510c7f2
José Lucas <jose.lukass03@gmail.com>
2021-06-26 18:28:51 -0300

    :recycle: Refactor user entity, create utilities methods

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -4,8 +4,13 @@ import {
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  BeforeUpdate,
+  BeforeInsert,
+  AfterLoad,
 } from 'typeorm';
 
+import bcrypt from 'bcrypt';
+import { Exclude } from 'class-transformer';
 import { v4 as uuid4 } from 'uuid';
 
 @Entity({ name: 'users' })
@@ -20,6 +25,7 @@ export class User {
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
 
   @Column({
@@ -33,9 +39,49 @@ export class User {
   @UpdateDateColumn()
   updatedAt: Date;
 
+  /**
+   * @description Encryption salt (bcrypt)
+   */
+  private readonly hashSalt: 10;
+
+  @Exclude()
+  private temporaryPassword: string;
+
   constructor() {
     if (!this.id) {
       this.id = uuid4();
+    }
+  }
+
+  /**
+   * @description Compare passwords (bcrypt)
+   */
+  public async compare(plain: string | Buffer) {
+    return bcrypt.compare(plain, this.password);
+  }
+
+  @AfterLoad()
+  public createTemporaryPassword() {
+    this.temporaryPassword = this.password;
+  }
+
+  @BeforeUpdate()
+  @BeforeInsert()
+  public async hash() {
+    if (this.temporaryPassword === this.password) {
+      return false;
+    }
+
+    try {
+      const salt = await bcrypt.genSalt(this.hashSalt);
+
+      const hash = await bcrypt.hash(this.password, salt);
+
+      this.password = hash;
+
+      return true;
+    } catch (error) {
+      console.warn(error);
     }
   }
 }
